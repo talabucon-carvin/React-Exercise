efa4c4aa2a91ef3445363dc7e1c1ca6990cb9e5a
carvintalabucontelus <86615789+carvintalabucontelus@users.noreply.github.com>
2022-04-20 21:50:32 +0800

    MUI added

--- src/App.js
+++ src/App.js
@@ -1,40 +1,97 @@
 
+//Hooks
+import { useEffect, useState } from 'react'; 
+
+//Components
 import UserInput from "./components/UserInput";
 import UserOutput from "./components/UserOutput";
-import { useEffect, useState } from 'react'; 
+
+//Material UI Components
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
+import Paper from '@mui/material/Paper';
+import Typography from '@mui/material/Typography';
+import Card from '@mui/material/Card';
+import Grid from '@mui/material/Grid';
 
 function App() {
 
+
+  //States
   const [ userName, setUserName ] = useState("User Name");
   const [ numberOfParagraphs, setNumberOfParagraphs ] = useState(0);
+  
+  const [ userOutputContainer, setUserOutputContainer ] = useState([])
+  const [ userOutputCollectionRender, setUserOutputCollectionRender ] = useState([])
+
+  //Handler
   const handleUserNameChange = (e)=>{setUserName(e.target.value)};
   const handleNumberOfParagraphsChange = (e)=>{setNumberOfParagraphs(e.target.value)};
   const handleSubmit = (e) => {e.preventDefault()};
-  const [ iterate, setIterate ] = useState([])
   
+  
+  //Component UserOutput Collection
   useEffect(()=>{
-    setIterate([])
+    setUserOutputContainer([])
     for(let i = 0; i < numberOfParagraphs; i++){
-      setIterate( arr => [...arr, <UserOutput key={i} userName={userName}/>])
+      setUserOutputContainer( arr => [...arr, <UserOutput key={i} userName={userName}/>])
     }
-  },[numberOfParagraphs,userName])
+  },[userOutputContainer, userName])
 
-
-  return (
-  <>
-    <UserInput 
-      userName={userName}
-      numberOfParagraphs={numberOfParagraphs}
-      handleUserNameChange={handleUserNameChange}
-      handleNumberOfParagraphsChange={handleNumberOfParagraphsChange}
-      handleSubmit={handleSubmit}
-    />
-    {
-      iterate
+  useEffect(()=>{
+    setUserOutputCollectionRender([])
+    for(let i = 0; i < userOutputContainer.length; i+=2){
+      for(let j = 0; j < 2; j++) {
+        <Card>
+          
+        </Card>
+      }
     }
     
+  },[userOutputContainer])
 
-  </>
+  return (
+    <>
+    <Container>
+      <Box sx={{ mt: 3 }}>
+        <UserInput 
+        userName={userName}
+        numberOfParagraphs={numberOfParagraphs}
+        handleUserNameChange={handleUserNameChange}
+        handleNumberOfParagraphsChange={handleNumberOfParagraphsChange}
+        handleSubmit={handleSubmit}
+        />
+        <Typography variant="h4" component="div" gutterBottom>
+        Simple Cards
+        </Typography>
+        <Paper sx={{ mt: 3 }} elevation={3}>
+          <Container>
+            <Box sx={{ py: 1 }}>
+            {
+            userOutputContainer
+            }
+            </Box>
+            <Grid container spacing={2}>
+              <Grid item xs={6}>
+                <Card>
+
+                </Card>
+              </Grid>
+
+              <Grid item xs={6}>
+                <Card>
+                  
+                </Card>
+              </Grid>
+              
+              
+            </Grid>
+          </Container>
+        </Paper>
+
+      </Box>
+    </Container>
+    </>
   );
 }
 
--- src/components/UserInput.js
+++ src/components/UserInput.js
@@ -6,29 +6,30 @@ const UserInput = ({
     handleUserNameChange,
     handleNumberOfParagraphsChange,
     handleSubmit
-    }) => 
-{
+}) => {
     return (
         <>
-            <form onSubmit={handleSubmit}>
-                <label> User Name:
-                    <input
-                        type="text"
-                        value={userName}
-                        onChange={handleUserNameChange}
-                    />
-                </label>
-                <label> Number of Paragraphs
-                    <input
-                        type="number"
-                        value={numberOfParagraphs}
-                        onChange={handleNumberOfParagraphsChange}
-                        min="0"
-                        max="3000"
-                    />
-                </label>
-            </form>
-
+        <form onSubmit={handleSubmit} style={{
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center'}}>
+            <label> User Name:
+                <input
+                    type="text"
+                    value={userName}
+                    onChange={handleUserNameChange}
+                />
+            </label>
+            <label> Number of Paragraphs
+                <input
+                    type="number"
+                    value={numberOfParagraphs}
+                    onChange={handleNumberOfParagraphsChange}
+                    min="0"
+                    max="3000"
+                />
+            </label>
+        </form>
         </>
     )
 };
